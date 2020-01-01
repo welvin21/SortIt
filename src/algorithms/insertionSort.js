7a6547ae6423de24127e42eb3c6f23c8cd89f4c6
welvin21 <47080921+welvin21@users.noreply.github.com>
2020-01-01 15:14:39 +0800

    Quicksort (#2)
    * Add quicksort
    
    * Modify App.js
    
    * Change slider range and step

--- src/algorithms/insertionSort.js
+++ src/algorithms/insertionSort.js
@@ -1,22 +1,25 @@
-const insertionSort = arr => {
-  for (let i = 1; i < arr.length; i++) 
-  {
-    if (arr[i] < arr[0]) {
-      arr.unshift(arr.splice(i,1)[0]);
-      return arr;
+const insertionSort = async(arr) => {
+  let frames = [];
+  let tmp = [...arr];
+  let len = tmp.length;
+  for (let i = 1; i < len; i++){
+    if (tmp[i] < tmp[0]) {
+      tmp.unshift(tmp.splice(i,1)[0]);
+      frames.push([...tmp]);
     } 
-    else if (arr[i] > arr[i-1]){
+    else if (tmp[i] > tmp[i-1]){
       continue;
     } 
     else {
       for (let j = 1; j < i; j++) {
-        if (arr[i] > arr[j-1] && arr[i] < arr[j]){
-          arr.splice(j,0,arr.splice(i,1)[0]);
-          return arr;
+        if (tmp[i] > tmp[j-1] && tmp[i] < tmp[j]){
+          tmp.splice(j,0,tmp.splice(i,1)[0]);
+          frames.push([...tmp]);
         }
       }
     }
   }
+  return frames;
 };
 
 export default insertionSort;
--- src/algorithms/quickSort.js
+++ src/algorithms/quickSort.js
@@ -1,8 +1,43 @@
-const quickSort = arr => {
-  //TODO : implement quickSort sorting algorithm
-  const newArr = [...arr];
-  newArr.sort();
-  return newArr;
+const partition = (frames, tmp, left, right) => {
+  let pivot   = tmp[Math.floor((right + left) / 2)],
+      i       = left,
+      j       = right;
+
+  while (i <= j) {
+    while(tmp[i] < pivot){
+      i++;
+    }
+    while(tmp[j] > pivot){
+      j--;
+    }
+    if(i <= j) {
+      tmp[j] = tmp.splice(i, 1, tmp[j])[0];
+      frames.push([...tmp]);
+      i++;
+      j--;
+    }
+  }
+  return i;
+}
+
+const main = async(frames, tmp, len, left, right) => {
+  let index;
+  if (len > 1){
+    index = partition(frames, tmp, left, right);
+    if(left < index - 1){
+      main(frames, tmp, len, left, index - 1);
+    }
+    if(index < right){
+      main(frames, tmp, len, index, right);
+    }
+  }
+}
+const quickSort = async(arr) => {
+  let frames = [];
+  let tmp = [...arr];
+  let len = tmp.length;
+  main(frames, tmp, len, 0, len-1).then(() => {});
+  return frames;
 };
 
 export default quickSort;
