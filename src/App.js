9949910e6502bb81eb6a4da004c0f0c40dc3da1c
welvin21 <47080921+welvin21@users.noreply.github.com>
2020-01-07 23:19:00 +0800

    Adds introduction modal (#11)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState }  from 'react';
+import { Intro } from './components/Intro';
 import { Sort } from './components/Sort';
 import './App.css';
 
 export const App = () => {
+  const [ isOpen, setOpen ] = useState(true);
   return(
+    <>
+      <Intro isOpen={isOpen} closeModal={() => setOpen(false)}/>
       <Sort/>
+    </>
   );
 }
